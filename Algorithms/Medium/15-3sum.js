cd123125376842a4071edcf36275108e614bb7b8
Lyrisbee <Lyrisbee@users.noreply.github.com>
2019-07-20 08:42:38 +0800

    add 15-3sum.js ver.2
    Runtime: 172 ms
    Memory Usage: 47.5 MB

--- Algorithms/Medium/15-3sum.js
+++ Algorithms/Medium/15-3sum.js
@@ -1,7 +1,12 @@
 /**
+ * Description
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ *
  * @param {number[]} nums
  * @return {number[][]}
  */
+ 
 var threeSum = function(nums) {
 
     const
@@ -10,47 +15,42 @@ var threeSum = function(nums) {
         result = [];
 
     let
-        left = 0, mid = 1, right = len - 1;
+        left = 0, mid = 1;
 
-    // at least there elements
-    if (len < 3) {
+    nums.sort((a, b) => a - b);
+    
+    // 至少三個元素，且第一位必須為負數
+    if (len < 3 || nums[0] > 0) {
         return [];
     }
 
-    nums.sort((a, b) => a - b);
-    for (left = 0; left < len; left ++) {
+    for (left = 0; left + 2 < len; left ++) {
 
-        if (nums[left] > 0) {
-            break;
-        }
-
-        if (nums[left] === 0 && left + 2 < len) {
-
-            if (nums[left + 1] === 0 && nums[left + 2] === 0) {
-                result.push([0, 0, 0]);
-            }
-
-            break;
-        }
-
-        for (mid = left + 1; mid < len; mid ++) {
+        for (mid = left + 1; mid + 1 < len; mid ++) {
 
             if (nums[left] + nums[mid] > 0) {
                 break;
             }
 
+			// 找出符合 left + mid = target
             let target = (nums[left] + nums[mid]) * -1;
 
-            if (nums[mid] < target && numSet.has(target)) {
+            // 檢查是否有 target 值存在，又因 set 相同值只會存一次，故需要另外檢查 mid 和 target 相同值的狀況
+            if (nums[mid] < target && numSet.has(target) || nums[mid] === target && nums[mid] === nums[mid + 1]) {
                 result.push([nums[left], nums[mid], target]);
             }
 
-            if (nums[mid] === target && mid + 1 < len && nums[mid] === nums[mid + 1]) {
-                result.push([nums[left], nums[mid], target]);
+            //略過相同的 mid 數，避免下輪拿到相同的值
+            while (mid + 1 < len && nums[mid] === nums[mid + 1]) {
+                mid ++;
             }
-
+        }
+        
+		//略過相同的 left 數，避免下輪拿到相同的值
+        while (left + 2 < len && nums[left] === nums[left + 1]) {
+                left ++;
         }
     }
 
-    return result.filter(( t = {}, a => !( t[a] = a in t) ));
+    return result;
 };
