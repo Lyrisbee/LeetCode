737cf9ae770b4fed01f4c2eac30d1f46d01b902c
Lyrisbee <Lyrisbee@users.noreply.github.com>
2019-07-21 13:18:36 +0800

    add 5133-Maximum-of-Absolute-Value-Expression.js

---
+++ Algorithms/Medium/5130-Number-of-Equivalent-Domino-Pairs.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} dominoes
+ * @return {number}
+ */
+var numEquivDominoPairs = function(dominoes) {
+    const len = dominoes.length;
+    
+    let count = 0;
+    
+    for(let i = 0; i < len - 1; i++) {
+
+        for(let j = i + 1; j < len; j++) {
+            let a = dominoes[i][0],
+                b = dominoes[i][1],
+                c = dominoes[j][0],
+                d = dominoes[j][1];
+
+            if (a == c && b == d || a ==d && b == c) {
+                count ++;
+            }
+        }
+    }
+    
+    return count;
+};
