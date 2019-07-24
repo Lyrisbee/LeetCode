8ee3ac5b7f1e66d04ecf2672482adc87915fb0f7
Lyrisbee <lyrisbee@protonmail.com>
2019-07-24 12:01:34 +0800

    add 5-Longest-Palindromic-Substring.js

---
+++ Algorithms/Medium/5-Longest-Palindromic-Substring.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+/**
+ * 說明
+ * 假設題目 "abcba"
+ * P[left][right] 是指 left 到 right 為迴文
+ *
+ * P[0][0] = true
+ * P[0][1] = s[0] === s[1]
+ * P[0][2] = s[0] === s[2] && P[0][1]
+ * ...
+ * P[1][0] = not exist
+ * ...
+ * P[1][4] = s[1] === s[4] && P[2][3]
+ *
+ * 以此類推
+ *
+ * 回文需要符合
+ * 1. s[left] === s[right]
+ * 2. P[left+1] === P[right-1] 或 left + 1 >= right -1 ([0][0], [0][1] 只要檢查值是否一樣)
+ */
+var longestPalindrome = function(s) {
+    const
+        len = s.length,
+        isPalindromic = [...Array(len)].map(x => Array(len).fill(false)) ;
+
+    let
+        start = 0,
+        max = 1;
+
+    for (let right = 1; right < len; right ++) {
+        for (let left = right - 1; left >= 0; left --) {
+            if ((left + 1 >= right - 1 || isPalindromic[left+1][right-1]) && s.substr(left, 1) === s.substr(right, 1)) {
+                isPalindromic[left][right] = true;
+                if (right - left + 1 > max) {
+                    max = right - left + 1;
+                    start = left;
+                }
+            }
+        }
+    }
+
+    return s.substr(start, max);
+};
--- README.md
+++ README.md
@@ -5,8 +5,9 @@ LeetCode
 
 | # | Title | Solution | Difficulty |
 | :---: | :-----: | :--------: | :----------: |
-|15|[3 Sum](https://leetcode.com/problems/3sum/)| [JS](./Algorithms/Medium/15-3sum.js)|Meduim|
+|15|[3 Sum](https://leetcode.com/problems/3sum/)| [JS](./Algorithms/Medium/15-3sum.js)|Medium|
 |7|[Reverse Integer](https://leetcode.com/problems/reverse-integer/)| [JS](./Algorithms/Easy/7-reverse-integer.js)|Easy|
+|5|[Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/)| [JS](./Algorithms/Medium/5-Longest-Palindromic-Substring.js)|Medium|
 |4|[Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/)| [JS](./Algorithms/Hard/4-median-of-two-sorted-arrays.js)|Hard|
 
 ### LeetCode Contest
