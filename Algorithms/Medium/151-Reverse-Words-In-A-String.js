e99763011369bbcbb2896e5760b3278db9f2771c
Lyrisbee <lyrisbee@protonmail.com>
2019-08-02 14:56:21 +0800

    prettier format

--- Algorithms/Medium/151-Reverse-Words-In-A-String.js
+++ Algorithms/Medium/151-Reverse-Words-In-A-String.js
@@ -3,5 +3,9 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s.split(' ').filter(s => s.length > 0).reverse().join(' ');
+    return s
+        .split(' ')
+        .filter((s) => s.length > 0)
+        .reverse()
+        .join(' ');
 };
